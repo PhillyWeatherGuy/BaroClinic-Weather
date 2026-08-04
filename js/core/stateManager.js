152361049177c811ef03b8e724cbbaed0cd4adff
PhillyWeatherGuy <andrew.n.orsini@gmail.com>
2026-08-04 12:50:22 -0400

    Create stateManager.js

---
+++ js/core/stateManager.js
@@ -0,0 +1,8 @@
+export const stateManager = {
+    BASE_URL: 'https://phillyweatherguy.github.io/BaroClinic-Weather---Backend/run_conus/',
+    manifest: null,
+    loadedChunkBitmaps: [],
+    globalSteps: [],
+    activeFrameState: null,
+    currentStepIndex: 0
+};
