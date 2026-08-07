43c0952cebdf52c91ef5b7f019cc392793dc1e5b
PhillyWeatherGuy <andrew.n.orsini@gmail.com>
2026-08-07 12:55:31 -0400

    Update stateManager.js

--- js/core/stateManager.js
+++ js/core/stateManager.js
@@ -1,6 +1,7 @@
 export const stateManager = {
     BASE_URL: 'https://baroclinic-data-proxy.andrew-n-orsini.workers.dev/', 
     manifest: null,
+    initTime: null, // 🌟 ADDED THIS
     loadedChunkBitmaps: [],
     globalSteps: [],
     activeFrameState: null,
