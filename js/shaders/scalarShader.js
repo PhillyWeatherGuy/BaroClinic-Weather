893943522a8d9faff8a80dcad020f530eb3c4881
PhillyWeatherGuy <andrew.n.orsini@gmail.com>
2026-08-14 13:10:10 -0400

    Update scalarShader.js

--- js/shaders/scalarShader.js
+++ js/shaders/scalarShader.js
@@ -1,6 +1,6 @@
 // js/shaders/scalarShader.js
-// COME BACK TO THIS IF SOMETHING BREAKS
-import { getPaletteForParameter, TEMP_PALETTE } from '../config/palettes.js';
+import { getPaletteForParameter, TEMP_PALETTE, PRECIP_PALETTE } from '../config/palettes.js';
+import { stateManager } from '../core/stateManager.js';
 
 const vsSource = `
     attribute vec2 a_pos;
@@ -13,7 +13,7 @@ const vsSource = `
 `;
 
 const fsSource = `
-    precision mediump float;
+    precision highp float;
     varying vec2 v_texcoord;
     uniform sampler2D u_dataTexture;
     uniform sampler2D u_paletteTexture;
@@ -22,17 +22,21 @@ const fsSource = `
     uniform vec2 u_uvScale;
 
     void main() {
-        // Mercator UV coordinate transform for Equirectangular input images
         float mercY = (0.5 - v_texcoord.y) * 6.28318530718;
         float latRad = 2.0 * atan(exp(mercY)) - 1.57079632679;
         float normY = clamp(0.5 - (latRad / 3.14159265359), 0.0, 1.0);
 
-        // 🌟 Fixed: normY aligns North Pole to Top of 2D Map
-        vec2 wrapped_uv = vec2(fract(v_texcoord.x), normY);
-        vec2 sprite_uv = u_uvOffset + wrapped_uv * u_uvScale;
+        vec2 tile_uv = vec2(fract(v_texcoord.x), normY);
+        vec2 sprite_uv = u_uvOffset + tile_uv * u_uvScale;
 
         float rawVal = texture2D(u_dataTexture, sprite_uv).r;
         vec4 color = texture2D(u_paletteTexture, vec2(rawVal, 0.5));
+        
+        // 🌟 Discard dry land / transparent pixels
+        if (color.a == 0.0) {
+            discard;
+        }
+
         gl_FragColor = vec4(color.rgb, color.a * u_opacity);
     }
 `;
@@ -41,13 +45,17 @@ function createPaletteTexture(gl, paletteHexArray = TEMP_PALETTE) {
     const paletteTex = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, paletteTex);
     const paletteData = new Uint8Array(paletteHexArray.length * 4);
+    const isPrecip = (paletteHexArray === PRECIP_PALETTE);
+    
     paletteHexArray.forEach((hex, i) => {
         const num = parseInt(hex.replace('#', ''), 16);
         paletteData[i * 4]     = (num >> 16) & 255;
         paletteData[i * 4 + 1] = (num >> 8) & 255;
         paletteData[i * 4 + 2] = num & 255;
-        paletteData[i * 4 + 3] = 255;
+        // 🌟 Make index 0 (0.00" rain) transparent so basemap shows through
+        paletteData[i * 4 + 3] = (isPrecip && i === 0) ? 0 : 255;
     });
+
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, paletteHexArray.length, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, paletteData);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
@@ -77,14 +85,16 @@ export function createScalarShaderLayer(mapInstance) {
             this.activeTex = null;
         },
 
-        /**
-         * 🌟 DYNAMIC PALETTE SWAP: Swaps GPU palette texture when picking a new parameter
-         */
         updatePalette: function(paramIdOrHexArray) {
             if (!this.gl) return;
-            const hexArray = Array.isArray(paramIdOrHexArray) 
-                ? paramIdOrHexArray 
-                : getPaletteForParameter(paramIdOrHexArray);
+            let hexArray;
+            if (Array.isArray(paramIdOrHexArray)) {
+                hexArray = paramIdOrHexArray;
+            } else if (paramIdOrHexArray === 'tp' || paramIdOrHexArray === 'precip') {
+                hexArray = PRECIP_PALETTE;
+            } else {
+                hexArray = getPaletteForParameter(paramIdOrHexArray);
+            }
             
             if (this.paletteTex) {
                 this.gl.deleteTexture(this.paletteTex);
@@ -128,7 +138,9 @@ export function createScalarShaderLayer(mapInstance) {
             gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
             gl.bufferData(gl.ARRAY_BUFFER, quadVertices, gl.STATIC_DRAW);
 
-            this.paletteTex = createPaletteTexture(gl, TEMP_PALETTE);
+            // 🌟 Load PRECIP_PALETTE if active param is 'tp', otherwise TEMP_PALETTE
+            const initialPalette = (stateManager.activeParam === 'tp') ? PRECIP_PALETTE : TEMP_PALETTE;
+            this.paletteTex = createPaletteTexture(gl, initialPalette);
         },
         
         preloadChunkTexture: function(chunkIndex, imageBitmap) {
@@ -167,7 +179,7 @@ export function createScalarShaderLayer(mapInstance) {
             gl.uniform1i(this.uPaletteTexture, 1);
 
             gl.uniformMatrix4fv(this.uMatrix, false, matrix);
-            gl.uniform1f(this.uOpacity, 1.00);
+            gl.uniform1f(this.uOpacity, 0.85); 
             gl.uniform2f(this.uUvOffset, this.uvOffset[0], this.uvOffset[1]);
             gl.uniform2f(this.uUvScale, this.uvScale[0], this.uvScale[1]);
 
